aaa725f27089365dfbfac4528bb2964994bb7e77
Katsiaryna Shymanovich <k.shymanovich@godeltech.com>
2021-10-11 13:56:48 +0300

    FixedDictionary

--- DictionaryTask1/DictionaryTask1/Program.cs
+++ DictionaryTask1/DictionaryTask1/Program.cs
@@ -1,33 +1,34 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 // 5. дан словарь (ключ - целое, значение - целое). Подсчитать сумму ключей неотрицательных значений
 namespace DictionaryTask1
 {
     class Dictionary
     {
-        public void NonNegKeySum(SortedList dc, int nel, int keysum = 0)
+        public void NonNegKeySum(IDictionary<int, int> dc) // Counting sum of non-negative keys
         {
-            for (int i = 0; i < nel; i++)
+            int keysum = 0;
+            for (int i = 0; i < dc.Count; i++)
             {
-                if ((int)dc.GetByIndex(i) >= 0)
+                if (dc.ElementAt(i).Key >= 0)
                     {
-                        keysum += (int)dc.GetKey(i);
-                    }
+                    keysum += dc.ElementAt(i).Key;
+                }
             }
         Console.WriteLine("Sum of the keys of non-negative dictionary elements is: " + keysum);
         }
     }
 
-
-
-   
+       
     class Program
     {
         static void Main(string[] args)
         {
             //creating dictionary of numeric elements
-            SortedList dc = new();
 
+            IDictionary<int, int> dc = new Dictionary<int, int>();
+            
             Console.WriteLine("Enter a number of elements in dictionary:");
 
             int nel = int.Parse(Console.ReadLine()); //user defines the number of elements in the dictionary
@@ -38,10 +39,11 @@ namespace DictionaryTask1
             int d;
             int k;
             Random r = new Random();
-            
+
             for (int i = 0; i < nel; i++)
             {
-                k = r.Next(-1000, 1000);
+                do {k = r.Next(-1000, 1000);}
+                while (dc.ContainsKey(k)); 
                 d = r.Next(-1000, 1000);
                 dc.Add(k, d);
                 Console.Write(k + ": ");
@@ -49,7 +51,7 @@ namespace DictionaryTask1
             }
 
             Dictionary dc1 = new();
-            dc1.NonNegKeySum(dc, nel);
+            dc1.NonNegKeySum(dc);
 
         }
     }
