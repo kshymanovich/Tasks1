65a35005431bde8783453153e393e86a31e01d37
Katsiaryna Shymanovich <k.shymanovich@godeltech.com>
2021-10-14 13:20:26 +0300

    FileDict1

--- FileTask1/Program.cs
+++ FileTask1/Program.cs
@@ -8,10 +8,10 @@ namespace FileTask1
 {
     class Dict
     {
-        public void BuildDict(string filename)
+        public ArrayList ReadFile(string filename)
         {
             // Reading a file into ArrayList
-            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dict.csv");
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename);
             using (StreamReader sr = File.OpenText(path))
             {
                 string s;
@@ -24,50 +24,58 @@ namespace FileTask1
                     Console.WriteLine(str[b]);
                     b++;
                 }
+                return str;
+            }
+        }
 
-                // Getting Keys for a Dictionary
-                string row = (string)str[0];
-                string[] strsplit = row.Split(';');
+        public IList<IDictionary<string, string>> BuildList(ArrayList str)
+        {
+            // Getting Keys for a Dictionary
+            string row = (string)str[0];
+            string[] strsplit = row.Split(';');
 
-                string key1 = strsplit[0];
-                string key2 = strsplit[1];
+            string key1 = strsplit[0];
+            string key2 = strsplit[1];
 
-                // Building a List of Dictionaries
-                var dict = new List<IDictionary<string, string>>();
-                string val1;
-                string val2;
-                for (int i = 1; i < str.Count; i++)
-                {
-                    IDictionary<string, string> dc = new Dictionary<string, string>();
-                    row = (string)str[i];
-                    strsplit = row.Split(';');
-                    val1 = strsplit[0];
-                    val2 = strsplit[1];
-                    dc.Add(key1, val1);
-                    dc.Add(key2, val2);
-                    dict.Add(dc);
-                }
+            // Building a List of Dictionaries
+            IList dict = new List<IDictionary<string, string>>();
+            string val1;
+            string val2;
+            for (int i = 1; i < str.Count; i++)
+            {
+                IDictionary<string, string> dc = new Dictionary<string, string>();
+                row = (string)str[i];
+                strsplit = row.Split(';');
+                val1 = strsplit[0];
+                val2 = strsplit[1];
+                dc.Add(key1, val1);
+                dc.Add(key2, val2);
+                dict.Add(dc);
+            }
+            return (IList<IDictionary<string, string>>)dict;
+        }
 
-                //Printing the List of Dictionaries down
-                Console.WriteLine();
-                Console.WriteLine("The List of Dictionaries is:");
-                for (int i = 0; i < dict.Count; i++)
-                {
-                    Console.WriteLine(dict[i].ElementAt(0).Key + " = " + dict[i].ElementAt(0).Value + "," + dict[i].ElementAt(1).Key + " =" + dict[i].ElementAt(1).Value);
-                }
+        public void PrintDict(IList<IDictionary<string, string>> dict)
+        {
+            //Printing the List of Dictionaries down
+            Console.WriteLine();
+            Console.WriteLine("The List of Dictionaries is:");
+            for (int i = 0; i < dict.Count; i++)
+            {
+                Console.WriteLine(dict[i].ElementAt(0).Key + " = " + dict[i].ElementAt(0).Value + "," + dict[i].ElementAt(1).Key + " = " + dict[i].ElementAt(1).Value);
             }
         }
     }
+
     class Program
     {
         static void Main(string[] args)
         {
 
-            string filename = "dict.csv";
-
             Dict dict1 = new();
-            dict1.BuildDict(filename);
-      
+            string filename = "dict.csv";
+            dict1.PrintDict(dict1.BuildList(dict1.ReadFile(filename)));
+
         }
     }
 }
